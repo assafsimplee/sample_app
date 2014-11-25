b8f214542af720b06ab4f4c8dfaf4b2ca69638b4
Assaf Zigdon <assaf@simplee.com>
2014-11-25 21:49:57 +0200

    log-in-log-out-exercises

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,17 +9,18 @@ class User < ActiveRecord::Base
                     uniqueness: { case_sensitive: false}
   has_secure_password
   validates :password, length: { minimum: 6 }
+  class << self
+    def User.digest(string)
+      cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                    BCrypt::Engine.cost
+      BCrypt::Password.create(string, cost: cost)
+    end
 
-  def User.digest(string)
-    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
-                                                  BCrypt::Engine.cost
-    BCrypt::Password.create(string, cost: cost)
-  end
-
-  def User.new_token
-    SecureRandom.urlsafe_base64
-  end
-
+    def User.new_token
+      SecureRandom.urlsafe_base64
+    end
+  end  
+  
   def remember
     self.remember_token = User.new_token
     update_attribute(:remember_digest, User.digest(remember_token))
